79741948c7b187af6fbd45c818c50fb543e9dc49
Lope <Lope922@gmail.com>
2015-04-13 05:26:39 -0500

    got 3 day forecast working and displaying images and brief description. Only works with test link at this point.  Need to see if there is a more effecient way of making web requests for icons.

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -23,19 +23,18 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Button1 = New System.Windows.Forms.Button()
-        Me.forcastImageBox = New System.Windows.Forms.PictureBox()
+        Me.forcastImageBox1 = New System.Windows.Forms.PictureBox()
         Me.lblDay = New System.Windows.Forms.Label()
         Me.lblTemp = New System.Windows.Forms.Label()
-        Me.testLabel = New System.Windows.Forms.Label()
         Me.lblDescription2 = New System.Windows.Forms.Label()
         Me.lblDay2 = New System.Windows.Forms.Label()
-        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.lblDay4 = New System.Windows.Forms.Label()
-        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
-        CType(Me.forcastImageBox, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.forcastImageBox2 = New System.Windows.Forms.PictureBox()
+        Me.lblDescription3 = New System.Windows.Forms.Label()
+        Me.lblDay3 = New System.Windows.Forms.Label()
+        Me.forcastImageBox3 = New System.Windows.Forms.PictureBox()
+        CType(Me.forcastImageBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.forcastImageBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.forcastImageBox3, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Button1
@@ -47,16 +46,16 @@ Partial Class Form1
         Me.Button1.Text = "Button1"
         Me.Button1.UseVisualStyleBackColor = True
         '
-        'forcastImageBox
+        'forcastImageBox1
         '
-        Me.forcastImageBox.BackColor = System.Drawing.SystemColors.InactiveBorder
-        Me.forcastImageBox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.forcastImageBox.Location = New System.Drawing.Point(181, 81)
-        Me.forcastImageBox.Name = "forcastImageBox"
-        Me.forcastImageBox.Size = New System.Drawing.Size(130, 121)
-        Me.forcastImageBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
-        Me.forcastImageBox.TabIndex = 1
-        Me.forcastImageBox.TabStop = False
+        Me.forcastImageBox1.BackColor = System.Drawing.SystemColors.InactiveBorder
+        Me.forcastImageBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.forcastImageBox1.Location = New System.Drawing.Point(181, 81)
+        Me.forcastImageBox1.Name = "forcastImageBox1"
+        Me.forcastImageBox1.Size = New System.Drawing.Size(130, 121)
+        Me.forcastImageBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.forcastImageBox1.TabIndex = 1
+        Me.forcastImageBox1.TabStop = False
         '
         'lblDay
         '
@@ -76,15 +75,6 @@ Partial Class Form1
         Me.lblTemp.TabIndex = 3
         Me.lblTemp.Text = "Brief description"
         '
-        'testLabel
-        '
-        Me.testLabel.AutoSize = True
-        Me.testLabel.Location = New System.Drawing.Point(236, 278)
-        Me.testLabel.Name = "testLabel"
-        Me.testLabel.Size = New System.Drawing.Size(39, 13)
-        Me.testLabel.TabIndex = 4
-        Me.testLabel.Text = "Label1"
-        '
         'lblDescription2
         '
         Me.lblDescription2.AutoSize = True
@@ -103,81 +93,79 @@ Partial Class Form1
         Me.lblDay2.TabIndex = 6
         Me.lblDay2.Text = "Day "
         '
-        'PictureBox1
+        'forcastImageBox2
         '
-        Me.PictureBox1.BackColor = System.Drawing.SystemColors.InactiveBorder
-        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.PictureBox1.Location = New System.Drawing.Point(385, 81)
-        Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(130, 121)
-        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
-        Me.PictureBox1.TabIndex = 5
-        Me.PictureBox1.TabStop = False
+        Me.forcastImageBox2.BackColor = System.Drawing.SystemColors.InactiveBorder
+        Me.forcastImageBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.forcastImageBox2.Location = New System.Drawing.Point(385, 81)
+        Me.forcastImageBox2.Name = "forcastImageBox2"
+        Me.forcastImageBox2.Size = New System.Drawing.Size(130, 121)
+        Me.forcastImageBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.forcastImageBox2.TabIndex = 5
+        Me.forcastImageBox2.TabStop = False
         '
-        'Label3
+        'lblDescription3
         '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(693, 228)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(80, 13)
-        Me.Label3.TabIndex = 10
-        Me.Label3.Text = "lblBfiefDescrip3"
+        Me.lblDescription3.AutoSize = True
+        Me.lblDescription3.Location = New System.Drawing.Point(693, 228)
+        Me.lblDescription3.Name = "lblDescription3"
+        Me.lblDescription3.Size = New System.Drawing.Size(80, 13)
+        Me.lblDescription3.TabIndex = 10
+        Me.lblDescription3.Text = "lblBfiefDescrip3"
         '
-        'lblDay4
+        'lblDay3
         '
-        Me.lblDay4.AutoSize = True
-        Me.lblDay4.Location = New System.Drawing.Point(587, 45)
-        Me.lblDay4.Name = "lblDay4"
-        Me.lblDay4.Size = New System.Drawing.Size(29, 13)
-        Me.lblDay4.TabIndex = 9
-        Me.lblDay4.Text = "Day "
+        Me.lblDay3.AutoSize = True
+        Me.lblDay3.Location = New System.Drawing.Point(587, 45)
+        Me.lblDay3.Name = "lblDay3"
+        Me.lblDay3.Size = New System.Drawing.Size(29, 13)
+        Me.lblDay3.TabIndex = 9
+        Me.lblDay3.Text = "Day "
         '
-        'PictureBox2
+        'forcastImageBox3
         '
-        Me.PictureBox2.BackColor = System.Drawing.SystemColors.InactiveBorder
-        Me.PictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.PictureBox2.Location = New System.Drawing.Point(590, 81)
-        Me.PictureBox2.Name = "PictureBox2"
-        Me.PictureBox2.Size = New System.Drawing.Size(130, 121)
-        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
-        Me.PictureBox2.TabIndex = 8
-        Me.PictureBox2.TabStop = False
+        Me.forcastImageBox3.BackColor = System.Drawing.SystemColors.InactiveBorder
+        Me.forcastImageBox3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.forcastImageBox3.Location = New System.Drawing.Point(590, 81)
+        Me.forcastImageBox3.Name = "forcastImageBox3"
+        Me.forcastImageBox3.Size = New System.Drawing.Size(130, 121)
+        Me.forcastImageBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.forcastImageBox3.TabIndex = 8
+        Me.forcastImageBox3.TabStop = False
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(876, 492)
-        Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.lblDay4)
-        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.lblDescription3)
+        Me.Controls.Add(Me.lblDay3)
+        Me.Controls.Add(Me.forcastImageBox3)
         Me.Controls.Add(Me.lblDescription2)
         Me.Controls.Add(Me.lblDay2)
-        Me.Controls.Add(Me.PictureBox1)
-        Me.Controls.Add(Me.testLabel)
+        Me.Controls.Add(Me.forcastImageBox2)
         Me.Controls.Add(Me.lblTemp)
         Me.Controls.Add(Me.lblDay)
-        Me.Controls.Add(Me.forcastImageBox)
+        Me.Controls.Add(Me.forcastImageBox1)
         Me.Controls.Add(Me.Button1)
         Me.Name = "Form1"
         Me.Text = "Form1"
-        CType(Me.forcastImageBox, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.forcastImageBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.forcastImageBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.forcastImageBox3, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
     Friend WithEvents Button1 As System.Windows.Forms.Button
-    Friend WithEvents forcastImageBox As System.Windows.Forms.PictureBox
+    Friend WithEvents forcastImageBox1 As System.Windows.Forms.PictureBox
     Friend WithEvents lblDay As System.Windows.Forms.Label
     Friend WithEvents lblTemp As System.Windows.Forms.Label
-    Friend WithEvents testLabel As System.Windows.Forms.Label
     Friend WithEvents lblDescription2 As System.Windows.Forms.Label
     Friend WithEvents lblDay2 As System.Windows.Forms.Label
-    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
-    Friend WithEvents Label3 As System.Windows.Forms.Label
-    Friend WithEvents lblDay4 As System.Windows.Forms.Label
-    Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
+    Friend WithEvents forcastImageBox2 As System.Windows.Forms.PictureBox
+    Friend WithEvents lblDescription3 As System.Windows.Forms.Label
+    Friend WithEvents lblDay3 As System.Windows.Forms.Label
+    Friend WithEvents forcastImageBox3 As System.Windows.Forms.PictureBox
 
 End Class
--- Form1.vb
+++ Form1.vb
@@ -23,10 +23,25 @@ Public Class Form1
         Dim xmlDay As XmlNode = xmlWeatherReader.SelectSingleNode("//title")
         ''  //title as day of the week 
 
+        'trying to access a specific day in the returned list of title elements. The 3rd day is what i want. 
+        Dim xmlday2 As XmlNode = xmlWeatherReader.SelectSingleNode("//forecastday[3]/title")
+        Dim xmlNodeDay2url_icon As XmlNode = xmlWeatherReader.SelectSingleNode("//forecastday[3]/icon_url")
+        Dim xmlday2Description As XmlNode = xmlWeatherReader.SelectSingleNode("//forecastday[3]/icon")
+
+        lblDay2.Text = xmlday2.InnerText
+        lblDescription2.Text = xmlday2Description.InnerText
+
+        Dim xmlday3 As XmlNode = xmlWeatherReader.SelectSingleNode("//forecastday[5]/title")
+        Dim xmlNodeDay3url_icon As XmlNode = xmlWeatherReader.SelectSingleNode("//forecastday[5]/icon_url")
+        Dim xmlday3Description As XmlNode = xmlWeatherReader.SelectSingleNode("//forecastday[5]/icon")
+
+        lblDay3.Text = xmlday3.InnerText
+        lblDescription3.Text = xmlday3Description.InnerText
+
         '' //icon for a brief description of the weather 
         Dim xmlIconDescription As XmlNode = xmlWeatherReader.SelectSingleNode("//icon")
         '' //icon_url as the icon image to read from the stream 
-
+        MessageBox.Show("day 2 " + xmlday2.InnerXml)
         Dim xmlIconRequestURL As XmlNode = xmlWeatherReader.SelectSingleNode("//icon_url")
 
         lblDay.Text = xmlDay.InnerText
@@ -34,26 +49,49 @@ Public Class Form1
 
         lblTemp.Text = xmlIconDescription.InnerText
 
-        testLabel.Text = xmlIconRequestURL.InnerText
+        'testLabel.Text = xmlIconRequestURL.InnerText
         '' now get the image for the forecast and draw it to the picture box 
         Try
             Dim icon_url As String = xmlIconRequestURL.InnerText
+            Dim icon2 As String = xmlNodeDay2url_icon.InnerText
+            Dim icon3 As String = xmlNodeDay3url_icon.InnerText
+
+
             Dim iconWebRequest As WebRequest = WebRequest.Create(icon_url)
+            
             Dim webimageResonse As Stream = iconWebRequest.GetResponse.GetResponseStream()
 
             Dim weatherDay1Icon As Image = Image.FromStream(webimageResonse)
+            forcastImageBox1.Image = weatherDay1Icon
+
+            Dim iconWebRequest2 As WebRequest = WebRequest.Create(icon2)
+
+            Dim webimageResonse2 As Stream = iconWebRequest2.GetResponse.GetResponseStream()
+
+            Dim weatherDayIcon2 As Image = Image.FromStream(webimageResonse2)
+            forcastImageBox2.Image = weatherDayIcon2
+
+            Dim iconWebRequest3 As WebRequest = WebRequest.Create(icon3)
+
+            Dim webimageResonse3 As Stream = iconWebRequest3.GetResponse.GetResponseStream()
+
+            Dim weatherDayIcon3 As Image = Image.FromStream(webimageResonse3)
+            forcastImageBox3.Image = weatherDayIcon3
+
+          
+            
+
 
             'Dim g As Graphics
 
             'forcastImageBox = g.DrawImage(Image.FromStream(weatherDay1Icon), New Rectangle(0, 0, forcastImageBox.Width, forcastImageBox.Height))
-            forcastImageBox.Image = weatherDay1Icon
+
 
         Catch ex As WebException
             MessageBox.Show(ex.Message)
         End Try
     End Sub
 
-    Private Sub testLabel_Click(sender As Object, e As EventArgs) Handles testLabel.Click
 
-    End Sub
+
 End Class
